7fc5cf4a8339cf6981c57e8c5064154a3e3507d0
mahdi <mjalalimech@gmail.com>
2024-10-29 13:07:35 +0400

    use built-in Toaster to toast errors
    Toaster is on App component:
    <Toaster/>
     and toast is used when fetching the data. into the catch (err) {toast.error}

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header/Header";
+import LocationList from "./components/locationList/LocationList";
 
 
 function App() {
   return(
     <div>
+      <Toaster/>
       <Header/ >
+      <LocationList />
     </div>
   )
 }
