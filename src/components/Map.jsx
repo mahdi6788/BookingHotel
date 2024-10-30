87f0bd2d4002698066f7ec72e7b1723249d84cde
Mahdi Jalalifar <Mjalalimech@gmail.com>
2024-10-31 00:08:16 +0400

    point to each location and keep it in memory

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,35 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotel } from "./HotelsProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 function Map() {
   const { isLoading, hotels } = useHotel();
-  const [mapCenter, setMapCenter] = useState([20, 3]);
+  const [mapCenter, setMapCenter] = useState([50, 5]);
+  const [searchParams, setSearchParams] = useSearchParams()
+  const lat = searchParams.get("lat")
+  const lng = searchParams.get("lng")
+
+  useEffect(()=>{
+  if (lat && lng) setMapCenter([lat, lng])
+  }, [lat,lng])
+
   return (
     <div className="mapContainer">
       <MapContainer
         className="map"
         center={mapCenter}
         zoom={13}
-        scrollWheelZoom={true}
-      >
+        scrollWheelZoom={true}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; 
+          <a href="https://www.openstreetmap.org/copyright">
+          OpenStreetMap
+          </a> 
+          contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-
+        <ChangeCenter position={mapCenter}/>
         {/* info based on the hotels */}
         {hotels.map((item) => (
           <Marker key={item.id} position={[item.latitude, item.longitude]}>
@@ -30,3 +42,10 @@ function Map() {
 }
 
 export default Map;
+
+/// point to each location on map
+function ChangeCenter({position}){
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
