bb2e79fa3167168fd4e237f5ca5a040b82207219
mahdi <mjalalimech@gmail.com>
2024-11-01 13:17:06 +0400

    add states to save cityname and country

--- src/components/AddNewBookmark/AddNewBookmark.jsx
+++ src/components/AddNewBookmark/AddNewBookmark.jsx
@@ -1,40 +1,53 @@
-import { useNavigate } from "react-router-dom"
-import useUrlLocation from "../../hooks/useUrlLocation"
-
+import { useNavigate } from "react-router-dom";
+import useUrlLocation from "../../hooks/useUrlLocation";
+import { useState } from "react";
 
 function AddNewBookmark() {
-    /// back button
-    const navigate = useNavigate()
-    const handleBack = (e)=>{
-        e.preventDefault()
-        navigate(-1)
-    }
+  const [cityName, setCityName] = useState("");
+  const [country, setCountry] = useState("");
 
-    const [lat, lng] = useUrlLocation()
-    
+  const [lat, lng] = useUrlLocation();
 
- return(
+  const navigate = useNavigate();
+  const handleBack = (e) => {
+    e.preventDefault();
+    navigate(-1);
+  };
+
+
+  return (
     <div>
-        <h2>Add New Bookmark</h2>
-        <div className="form">
-            <div className="formControl">
-                <label htmlFor="cityName">City Name</label>
-                <input type="text" className="cityName" id="cityName" />
-            </div>
-            <div className="formControl">
-                <label htmlFor="country">Country</label>
-                <input type="text" className="country" id="country" />
-            </div>
-            <div className="buttons">
-            <div className="btn btn--back" onClick={handleBack}>&larr; Back</div>
-            <div className="btn btn--primary">Add</div>
-            </div>
-
+      <h2>Add New Bookmark</h2>
+      <div className="form">
+        <div className="formControl">
+          <label htmlFor="cityName">City Name</label>
+          <input
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+            type="text"
+            className="cityName"
+            id="cityName"
+          />
         </div>
-        
+        <div className="formControl">
+          <label htmlFor="country">Country</label>
+          <input
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            type="text"
+            className="country"
+            id="country"
+          />
+        </div>
+        <div className="buttons">
+          <div className="btn btn--back" onClick={handleBack}>
+            &larr; Back
+          </div>
+          <div className="btn btn--primary">Add</div>
+        </div>
+      </div>
     </div>
- )   
+  );
 }
 
-
-export default AddNewBookmark
+export default AddNewBookmark;
